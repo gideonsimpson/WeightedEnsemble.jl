35339a4183457dfcc03b1b64cdabc13d61d1b7d1
Gideon Simpson <gideon.simpson@gmail.com>
2020-08-16 21:42:38 -0400

    added observable version of prun_we.

--- src/we.jl
+++ src/we.jl
@@ -127,7 +127,7 @@ specified fucntion, `f`, along the trajecotry.
 * `selection!` - selection scheme
 * `analysis!` - perform any post mutation updates
 * `n_we_steps` - number of steps in the WE run
-* `save_trajectory=true` - save the ensemble and bins at each iteration.  if false, only returns the final state
+* `f` - Observable function for the ergodic average
 """
 function run_we_observable(E₀::TE, mutation::FM, selection!::FS, analysis!::FA, n_we_steps::Int, f::FO) where
    {TE<:AbstractEnsemble, FM<:Function, FS<:Function, FA<:Function, FO<:Function}
@@ -287,6 +287,73 @@ function prun_we(E₀::TE, mutation::FM, selection!::FS, analysis!::FA, n_we_ste
 
 end
 
+"""
+`prun_we_observable`: Run a parallel WE simulation, optionally returning the ensemble at
+each step. This performs the mutation steps in parallel, and assumes a worker
+pool has already been created.
+
+### Arguments
+* `E₀` - initial particle ensemble
+* `B₀` - initial bin data structure
+* `mutation` - mutation function
+* `selection!` - selection scheme
+* `rebin!` - rebin and update particles and bins
+* `n_we_steps` - number of steps in the WE run
+* `f` - Observable function for the ergodic average
+"""
+function prun_we_observable(E₀::TE, B₀::TB, mutation::FM, selection!::FS, rebin!::FR, n_we_steps::Int, f::FO) where
+   {TE<:EnsembleWithBins, TB<:AbstractBins, FM<:Function, FS<:Function, FR<:Function, FO<:Function}
+
+   E = deepcopy(E₀);
+   B = deepcopy(B₀);
+   f_trajectory = zeros(n_we_steps);
+
+   for t in 0:n_we_steps-1
+      # first selection is at t = 0
+      selection!(E, B, t);
+      @. E.ω = E.ω̂;
+      E.ξ .= pmap(mutation, E.ξ̂);
+      # after mutation, time is t ↦ t+1
+      rebin!(E, B, t+1);
+      f_trajectory[t+1] = f.(E.ξ) ⋅ E.ω;
+   end
+
+   return f_trajectory
+
+end
+
+"""
+`prun_we_observable`: Run a parallel WE simulation, optionally returning the ensemble at
+each step. This performs the mutation steps in parallel, and assumes a worker
+pool has already been created.
+
+### Arguments
+* `E₀` - initial particle ensemble
+* `mutation` - mutation function
+* `selection!` - selection scheme
+* `analysis!` - perform any post mutation updates
+* `n_we_steps` - number of steps in the WE run
+* `f` - Observable function for the ergodic average
+"""
+function prun_we_observable(E₀::TE, mutation::FM, selection!::FS, analysis!::FA, n_we_steps::Int, f::FO) where
+   {TE<:AbstractEnsemble, FM<:Function, FS<:Function, FA<:Function, FO<:Function}
+
+   E = deepcopy(E₀);
+   f_trajectory = zeros(n_we_steps);
+
+   for t in 0:n_we_steps-1
+      # first selection is at t = 0
+      selection!(E, B, t);
+      @. E.ω = E.ω̂;
+      E.ξ .= pmap(mutation, E.ξ̂);
+      # after mutation, time is t ↦ t+1
+      analysis!(E, t+1);
+      f_trajectory[t+1] = f.(E.ξ) ⋅ E.ω;
+   end
+
+   return f_trajectory
+
+end
 
 """
 `prun_we!`: Run an in place parallel WE simulation.  This performs the mutation
