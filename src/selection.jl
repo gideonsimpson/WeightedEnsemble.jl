fbc25ada0fd84295aabee3b1cf69d49e5c632bc7
Gideon Simpson <gideon.simpson@gmail.com>
2021-05-11 21:58:32 -0400

    added a targeted function selection

--- src/selection.jl
+++ src/selection.jl
@@ -1,4 +1,69 @@
 """
+`targeted_allocation_selection!`: Targeted allocation of particles using a
+specified function, `g`, such that the party allocation is proportional to `g ⋅
+ω` for each bin.
+
+### Arguments
+* `E` - particle ensemble
+* `B` - bin data structure
+* `g` - target function
+* `t` - t-th seletion step
+* `resample` - resampling scheme
+"""
+function targeted_allocation_selection!(E::TE, B::TB, g::F, t::Int; resample=Systematic) where{TE<:EnsembleWithBins, TB<:AbstractBins, F<:Function}
+
+   n_particles = length(E);
+   n_bins = length(B);
+   # zero out offspring counts
+   @. E.o = 0;
+   @. B.target = 0;
+
+   # identify nonempty bins
+   non_empty_bins = findall(n->n>0, B.n);
+   R = length(non_empty_bins);
+   Ñ = zeros(n_bins);
+   ρ = zeros(n_bins);
+
+   for p in non_empty_bins
+      particle_ids = findall(isequal(p), E.b);
+      @inbounds Ñ[p] = E.ω[particle_ids] ⋅ g.(E.ξ[particle_ids],t);
+   end
+
+   if(sum(Ñ)>0)
+      # compute probabilities
+      ρ .= Ñ/sum(Ñ);
+      B.target .= (B.n .>0) .+ resample(n_particles-R, ρ);
+
+      # compute number of offspring of each particle bin by bin
+      for p in non_empty_bins
+         # get particle indices for bin p
+         particle_ids = findall(isequal(p), E.b);
+         @inbounds E.o[particle_ids] .= resample(B.target[p], E.ω[particle_ids]/B.ν[p]);
+      end
+
+   else
+      # every particle copies itself
+      @. B.target = B.n;
+      @. E.o = 1;
+   end
+
+   # resample the particles
+   n_spawned = 0;
+   for i in 1:n_particles
+      # identify the bin of the current particle
+      @inbounds bin = E.b[i];
+      for k in 1:E.o[i]
+         @inbounds copy!(E.ξ̂[k+n_spawned], E.ξ[i]);
+         @inbounds E.ω̂[k+n_spawned] = B.ν[bin]/B.target[bin];
+         @inbounds E.b̂[k+n_spawned] = bin;
+      end
+      @inbounds n_spawned += E.o[i];
+   end
+   E, B
+end
+
+
+"""
 `optimal_allocation_selection!`: Optimally particles according to the bins,
 using a value function to approximate mutation variance.
 
